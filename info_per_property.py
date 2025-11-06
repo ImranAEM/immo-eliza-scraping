8ad00b2e3edb1b2d590e6b2f7c080c17ef2ae871
Michiel Rondelez <michiel.rondelez.pro@gmail.com>
2025-11-06 17:01:47 +0100

    csv

--- info_per_property.py
+++ info_per_property.py
@@ -4,29 +4,38 @@ import time
 from selenium.webdriver.common.by import By
 import re
 import json
+import csv
 
-
+from check_convert_data import clean_property_data_output
 def get_soup(url:str) -> tuple[BeautifulSoup,str]:
     # initiate driver and getting the page
     driver = webdriver.Firefox()
     driver.get(url) # opening the property's page
 
     # click cookies
-    time.sleep(2)
-    driver.find_element(By.XPATH, "//*[@id='didomi-notice-agree-button']").click()
-
+    time.sleep(1)
+    try:
+        driver.find_element(By.XPATH, "//*[@id='didomi-notice-agree-button']").click()
+    except Exception as e:
+        print(f"Error for clicking cookies   : {e} in url {url}")
     # get html
     soup = BeautifulSoup(driver.page_source, "html.parser")
     #get json for the meta data
     json_string = driver.execute_script(f"return window.localStorage.getItem('__property_details__');")
-
+    driver.close()
     driver.quit()
 
     return soup, json_string
 
 def get_info(my_tuple : tuple[BeautifulSoup, str]) -> dict:
     soup = my_tuple[0]
-    json_info = json.loads(my_tuple[1])
+    try:
+        json_info = json.loads(my_tuple[1])
+    except Exception as e:
+
+        print(f"Error : {e} in url {my_tuple[1]}")
+        return 
+    """
     property_info ={
         "Id" : json_info["reference"], 
         "Property type" : json_info["propertyType"], 
@@ -36,6 +45,17 @@ def get_info(my_tuple : tuple[BeautifulSoup, str]) -> dict:
         "Subtype" : json_info["propertySubType"],
         "Sale type" : json_info["transactionType"]
         }
+        """
+    property_info ={
+        "Id" : json_info["reference"] if json_info.get("reference") else None, 
+        "Property type" : json_info["propertyType"] if json_info.get("propertyType") else None, 
+        "Price" : json_info["price"] if json_info.get("price") else None, 
+        "Locality" : json_info["city"] if json_info.get("city") else None,
+        "Postcode" : json_info["zipCode"] if json_info.get("zipCode") else None,
+        "Subtype" : json_info["propertySubType"] if json_info.get("propertySubType") else None,
+        "Sale type" : json_info["transactionType"] if json_info.get("transactionType") else None
+        }
+
     for info_box in soup.find(class_="general-info-wrapper").find_all(class_="data-row-wrapper"):
         for info_name, info in zip(info_box.find_all("h4"), info_box.find_all("p")):
             property_info[re.sub(r"/\n","",info_name.text.strip())] = re.sub(r"/\n","", info.text.strip())#removes \n
@@ -43,24 +63,137 @@ def get_info(my_tuple : tuple[BeautifulSoup, str]) -> dict:
 
 def get_info_project(soup: BeautifulSoup) -> dict:
     pass
+"""
+def get_info_project(my_tuple : tuple[BeautifulSoup, str]) -> dict:
 
-def all_properties_info(properties: list[str]) -> dict:
-    properties_info = {}
+    json_info = json.loads(my_tuple[1])
+    print(json_info)
+
+    soup = my_tuple[0]
+    
+
+    info_project = {}
+
+    # TYPE OF PROPERTY
+    title = soup.find("h1").contents[0]
+    property_type = title.text.strip() if title else None
+    #print(property_type)
+
+    # ADDRESS
+
+    title_section = driver.find_element(By.CLASS_NAME, "detail__header_address")
+    address_section = title_section.find_element(By.CLASS_NAME, "d-none.d-md-block")
+
+    spans = address_section.find_elements(By.TAG_NAME, "span")
+    address = " ".join([span.text for span in spans])
+
+
+
+    
+    section = soup.find("div", class_="section-container py-2 mt-lg-2")
+    highlights = section.find_all("div", class_="property-highlight")
+
+    
+    # Amount of the rooms
+    for room in highlights:
+        if "Bedroom" in room.text:   
+            strong_tag = room.find("strong")
+            if strong_tag:
+                num_room = strong_tag.text  
+
+    
+    # The prize of the project
+    for amount in highlights:
+        if "€" in amount.text:
+            strong_tag = amount.find("strong")
+            price = strong_tag.text
+
+
+    # The space of the project in m²
+
+    for area in highlights:
+        if "m²" in area.text:   
+            strong_tag = area.find("strong")
+            if strong_tag:
+                area_m = strong_tag.text  
+
+
+    #info_project["Property type"] = property_type
+    #info_project["Address"] = address
+    info_project["Number of bedrooms"] = num_room
+    info_project["Livable surface"] = area_m
+    info_project["Price"] = price.replace('\u202f', ' ').strip()
+
+
+    return info_project
+"""
+
+def all_properties_info(properties: list[str]) -> list[dict]:
+    all_properties_info = list[dict]()
+    to_csv_titles() # write the titles to the csv file
     for property_url in properties:
-        #print(property_url)
-        if re.match(r"project", property_url): 
-            info = get_info_project(get_soup(property_url))
+        print(property_url)
+        if re.search(r"project", property_url): 
+            #info = get_info_project(get_soup(property_url))
+            pass
         else:
             info = get_info(get_soup(property_url))
-        properties_info[info["id"]] = info
+            print("error with info", property_url)
+                
+            cleaned_info = clean_property_data_output()))
+            if cleaned_info is not None:
+                all_properties_info.append(cleaned_info)
+                print(cleaned_info.values())
+                to_csv(cleaned_info)
+            else:
+                print(f"Error : {cleaned_info} in url {property_url}")
+        #properties_info[info["Id"]] = info
 
-    return properties_info
+    return all_properties_info
 
 
-list_of_properties = ['https://immovlan.be/en/detail/villa/for-sale/7180/seneffe/vwd15538', 'https://immovlan.be/en/detail/apartment/for-rent/1410/waterloo/vbd47706', 'https://immovlan.be/en/detail/residence/for-sale/7090/hennuyeres/rbu63284', 'https://immovlan.be/en/detail/residence/for-rent/6830/bouillon/vbd47666', 'https://immovlan.be/en/detail/duplex/for-rent/9300/aalst/rbu63264', 'https://immovlan.be/en/detail/apartment/for-rent/9150/bazel/rbu63097', 'https://immovlan.be/en/detail/mixed-building/for-sale/9688/schorisse/rbu63087', 'https://immovlan.be/en/detail/residence/for-sale/9630/zwalm/rbu62977', 'https://immovlan.be/en/detail/apartment/for-rent/2600/berchem/rbu62725', 'https://immovlan.be/en/detail/apartment/for-rent/2000/antwerp/rbu62723', 'https://immovlan.be/en/detail/residence/for-rent/8211/aartrijke/rbu62717', 'https://immovlan.be/en/detail/loft/for-rent/1080/sint-jans-molenbeek/vbd47427', 'https://immovlan.be/en/detail/residence/for-sale/5190/jemeppe-sur-sambre/vbd47422', 'https://immovlan.be/en/detail/apartment/for-sale/8210/zedelgem/rbu62613', 'https://immovlan.be/en/detail/apartment/for-sale/2840/rumst/rbu62593', 'https://immovlan.be/en/detail/residence/for-sale/8552/moen/rbu62561', 'https://immovlan.be/en/detail/residence/for-sale/8560/gullegem/rbu62532']
-#list_of_properties_projects = ['https://immovlan.be/en/projectdetail/24431-p-vk0101-tfjkrblr', 'https://immovlan.be/en/projectdetail/25733-4036945', 'https://immovlan.be/en/detail/villa/for-sale/7180/seneffe/vwd15538', 'https://immovlan.be/en/detail/apartment/for-rent/1410/waterloo/vbd47706', 'https://immovlan.be/en/detail/residence/for-sale/7090/hennuyeres/rbu63284', 'https://immovlan.be/en/detail/residence/for-rent/6830/bouillon/vbd47666', 'https://immovlan.be/en/detail/duplex/for-rent/9300/aalst/rbu63264', 'https://immovlan.be/en/detail/apartment/for-rent/9150/bazel/rbu63097', 'https://immovlan.be/en/detail/mixed-building/for-sale/9688/schorisse/rbu63087', 'https://immovlan.be/en/detail/residence/for-sale/9630/zwalm/rbu62977', 'https://immovlan.be/en/detail/apartment/for-rent/2600/berchem/rbu62725', 'https://immovlan.be/en/detail/apartment/for-rent/2000/antwerp/rbu62723', 'https://immovlan.be/en/detail/residence/for-rent/8211/aartrijke/rbu62717', 'https://immovlan.be/en/detail/loft/for-rent/1080/sint-jans-molenbeek/vbd47427', 'https://immovlan.be/en/detail/residence/for-sale/5190/jemeppe-sur-sambre/vbd47422', 'https://immovlan.be/en/detail/apartment/for-sale/8210/zedelgem/rbu62613', 'https://immovlan.be/en/detail/apartment/for-sale/2840/rumst/rbu62593', 'https://immovlan.be/en/detail/residence/for-sale/8552/moen/rbu62561', 'https://immovlan.be/en/detail/residence/for-sale/8560/gullegem/rbu62532']
+def to_csv_titles():
+    titles = [
+        "Property ID",
+        "Locality name",
+        "Postal code",
+        "Price", 
+        "Type of property", 
+        "Subtype of property", 
+        "Type of sale", 
+        "Number of rooms", 
+        "Living area", 
+        "Equipped kitchen", 
+        "Furnished", 
+        "Open fire", 
+        "Terrace", 
+        "Garden", 
+        "Number of facades",
+        "Swimming pool", 
+        "State of building"
+    ]
+    with open("data.csv", "w", newline='') as f:
+        output = csv.writer(f)
+        output.writerow(titles)
 
-#print(all_properties_info(list_of_properties))
+def to_csv(to_write: dict):   
+    with open("data.csv", "a") as f:
+        output = csv.writer(f)
+        output.writerow(to_write.values())
+        #for d in l:
+        # output.writerow(list(to_write.values()))
 
-print(get_info(get_soup('https://immovlan.be/en/detail/villa/for-sale/7180/seneffe/vwd15538')))
-#print(get_info(get_soup('https://immovlan.be/en/projectdetail/1485771-01267921_om_272263')))
+
+def run_over_all_url_properties_from_csv():
+    all_urls = []
+   
+    with open('url_list.txt', 'r') as file:
+        all_urls = [line.strip() for line in file if line.strip()]
+    print(len(all_urls))
+
+    properties = all_properties_info(all_urls)
+    print(len(properties))
+
+
+
+run_over_all_url_properties_from_csv()
